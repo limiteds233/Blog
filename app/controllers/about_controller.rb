7e6443607e6680f11f46b89b26c66e7b58a311b0
limiteds233 <ss2333_1@mail.ru>
2019-10-19 12:52:55 +0300

    add articles create

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,9 @@
+class AboutController < ApplicationController
+    def new
+    end
+
+    def create
+    end
+    
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-
+  resource :about
+  resource :terms
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
